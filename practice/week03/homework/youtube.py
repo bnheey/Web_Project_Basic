96f07a6168f19e377e54d2f9370c3281a349ff27
Heeeon <bbjoite09@naver.com>
2021-05-08 01:46:18 +0900

    ADD week03_homework 21/05/08 01:36

--- practice/week03/homework/youtube.py
+++ practice/week03/homework/youtube.py
@@ -1,4 +1,27 @@
-import os
-import googleapiclient.discovery
+from googleapiclient.discovery import build
 import secret
 
+comments = list()
+api_obj = build('youtube', 'v3', developerKey=secret.youtube_api)
+video_id = 'HKgkwG9HT_c&t'
+response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=100).execute()
+
+while response:
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+
+        print(comment, comment['authorDisplayName'])
+
+        if item['snippet']['totalReplyCount'] > 0:
+            for reply_item in item['replies']['comments']:
+                reply = reply_item['snippet']
+                comments.append(
+                    [reply['textDisplay'], reply['authorDisplayName'], reply['publishedAt'], reply['likeCount']])
+
+    if 'nextPageToken' in response:
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id,
+                                                 pageToken=response['nextPageToken'], maxResults=100).execute()
+    else:
+        break
+
+print(response)
