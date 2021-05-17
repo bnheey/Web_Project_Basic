ed4e1eb6fd980ad16c100afd11022a2500387223
Heeeon <bbjoite09@naver.com>
2021-05-17 23:41:41 +0900

    모두의 bookreview 완성 21/05/17 23:41

--- practice/bookreview/app.py
+++ practice/bookreview/app.py
@@ -17,14 +17,26 @@ def home():
 @app.route('/review', methods=['POST'])
 def write_review():
     # 1. 클라이언트가 준 title, author, review 가져오기.
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+
     # 2. DB에 정보 삽입하기
+    review = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+    db.reviews.insert_one(review)
+
     # 3. 성공 여부 & 성공 메시지 반환하기
-    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+    return jsonify({'result': 'success', 'msg': '등록되었습니다!'})
 
 
 @app.route('/review', methods=['GET'])
 def read_reviews():
-    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+    reviews = list(db.reviews.find({}, {"_id" : False}))
+    return jsonify({'result': 'success', 'reviews': reviews})
 
 
 if __name__ == '__main__':
