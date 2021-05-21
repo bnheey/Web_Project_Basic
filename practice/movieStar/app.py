0d0f86e8bd1703edd216c14be9393b57ac101cd0
Heeeon <bbjoite09@naver.com>
2021-05-22 00:47:57 +0900

    삭제 버튼에 대한 반응 처리 21/05/22 00:47

--- practice/movieStar/app.py
+++ practice/movieStar/app.py
@@ -42,7 +42,11 @@ def like_star():
 @app.route('/api/delete', methods=['POST'])
 def delete_star():
     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    name_receive = request.form['name_give']
+
     # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
+    db.mystar.delete_one({'name' : name_receive})
+
     # 3. 성공하면 success 메시지를 반환합니다.
     return jsonify({'result': 'success'})
 
