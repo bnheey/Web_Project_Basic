594b7e1e0cfee7f8808d72bc8dbec8eca0d1ee4b
Heeeon <bbjoite09@naver.com>
2021-05-22 00:42:15 +0900

    좋아요 버튼에 대한 반응 처리 21/05/22 00:42

--- practice/movieStar/app.py
+++ practice/movieStar/app.py
@@ -23,7 +23,21 @@ def show_stars():
 
 @app.route('/api/like', methods=['POST'])
 def like_star():
+    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    name_receive = request.form['name_give']
+
+    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
+    star = db.mystar.find_one({'name': name_receive})
+    # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
+    new_like = star['like'] + 1
+
+    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
+    # 참고: '$set' 활용하기!
+    db.mystar.update_one({'name': name_receive}, {'$set': {'like': new_like}})
+
+    # 5. 성공하면 success 메시지를 반환합니다.
     return jsonify({'result': 'success'})
+
 
 @app.route('/api/delete', methods=['POST'])
 def delete_star():
